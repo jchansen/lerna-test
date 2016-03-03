828ebd6a69966860fd89fd8ba610c83f1d0d6f03
Jason Hansen <jason_hansen@outlook.com>
2016-03-02 17:17:22 -0700

    change version requirement

--- packages/lerna-test-b/index.js
+++ packages/lerna-test-b/index.js
@@ -3,6 +3,6 @@ var file2 = require('./file2');
 
 module.exports = {
   hello: function () {
-    console.log('/packages/lerna-test-a/index.js 2');
+    console.log('/packages/lerna-test-a/index.js 4');
   }
 };
